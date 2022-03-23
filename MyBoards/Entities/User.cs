874b8348ce254f3c7d0ec4c9f90539b9fc0bbe44
Jakub Kozera <KOZERJ01@heiway.net>
2022-03-23 06:53:40 +0100

    3.3 Customowe migracje

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -10,8 +10,8 @@ namespace MyBoards.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+
+        public string FullName { get; set; }
         public string Email { get; set; }
 
         public Address Address { get; set; }
---
+++ MyBoards/Migrations/20220323054248_UserFullName.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class UserFullName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+               name: "FullName",
+               table: "Users",
+               type: "nvarchar(max)",
+               nullable: true);
+
+            migrationBuilder.Sql(@"
+                UPDATE Users
+                SET FullName = FirstName + ' ' + LastName
+            ");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+               name: "LastName",
+               table: "Users",
+               type: "nvarchar(max)",
+               nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            // update new columns
+
+            migrationBuilder.DropColumn(
+               name: "FullName",
+               table: "Users");
+        }
+    }
+}
