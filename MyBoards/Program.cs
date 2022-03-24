2fd80e2ad899743d7745abc973e0a8441e5e0a6f
Jakub Kozera <KOZERJ01@heiway.net>
2022-03-24 09:58:20 +0100

    3.4 Aplikowanie migracji

--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -18,4 +18,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetService<MyBoardsContext>();
+
+var pendingMigrations = dbContext.Database.GetPendingMigrations();
+if (pendingMigrations.Any())
+{
+    //dbContext.Database.Migrate();
+}
+
 app.Run();
