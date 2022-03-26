74e8982981064ea607136099d4f39a890244ebb3
Jakub Kozera <KOZERJ01@heiway.net>
2022-03-26 09:46:01 +0100

    4.2 Ręczne dostosywanie migracji

---
+++ MyBoards/Migrations/20220326083732_AdditionWorkItemStateSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class AdditionWorkItemStateSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "On Hold");
+
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "Rejected");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "On Hold");
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "Rejected");
+        }
+    }
+}
