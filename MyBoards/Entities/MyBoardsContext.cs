b4a7d36bfc532149a43efa3cc4fbd803ea17fe08
Jakub Kozera <KOZERJ01@heiway.net>
2022-03-26 07:34:28 +0100

    4.1 Model seed

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -96,6 +96,11 @@ namespace MyBoards.Entities
                 .HasOne(u => u.Address)
                 .WithOne(a => a.User)
                 .HasForeignKey<Address>(a => a.UserId);
+
+            modelBuilder.Entity<WorkItemState>()
+                .HasData(new WorkItemState() { Id = 1, Value = "To Do" },
+                new WorkItemState() { Id = 2, Value = "Doing" },
+                new WorkItemState() { Id = 3, Value = "Done" });
         }
     }
 }
---
+++ MyBoards/Migrations/20220326063134_WorkItemStateSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class WorkItemStateSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                columns: new[] { "Id", "Value" },
+                values: new object[] { 1, "To Do" });
+
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                columns: new[] { "Id", "Value" },
+                values: new object[] { 2, "Doing" });
+
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                columns: new[] { "Id", "Value" },
+                values: new object[] { 3, "Done" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
