54dfdca7dd387655caec5bc3a5b5a795a2e1dd1b
Jakub Kozera <KOZERJ01@heiway.net>
2022-03-28 07:41:52 +0200

    4.3 Customowa logika seedowania

--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MyBoards.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,7 +24,37 @@ var dbContext = scope.ServiceProvider.GetService<MyBoardsContext>();
 var pendingMigrations = dbContext.Database.GetPendingMigrations();
 if (pendingMigrations.Any())
 {
-    //dbContext.Database.Migrate();
+    dbContext.Database.Migrate();
+}
+
+var users = dbContext.Users.ToList();
+if(!users.Any())
+{
+    var user1 = new User()
+    {
+        Email = "user1@test.com",
+        FullName = "User One",
+        Address = new Address()
+        {
+            City = "Warszwa",
+            Street = "Szeroka"
+        }
+    };
+
+    var user2 = new User()
+    {
+        Email = "user2@test.com",
+        FullName = "User Two",
+        Address = new Address()
+        {
+            City = "Krakow",
+            Street = "Długa"
+        }
+    };
+
+    dbContext.Users.AddRange(user1, user2);
+
+    dbContext.SaveChanges();
 }
 
 app.Run();
