ded81854165be8edec33cf022cf4025a5cb0da56
Jakub Kozera <KOZERJ01@heiway.net>
2022-04-23 09:17:38 +0200

    7.4 Includes

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Northwind.Entities;
+using System.Linq.Expressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,13 +22,41 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("data", async (NorthwindContext db) =>
+app.MapGet("getOrderDetails", async (NorthwindContext db) =>
 {
-	var sampleData = await db.Products
-    .Take(100)
-    .ToListAsync();
+    Order order = await GetOrder(46, db, o => o.OrderDetails);
+    return new { OrderId = order.OrderId, Details = order.OrderDetails};
+});
 
-	return sampleData;
+app.MapGet("getOrderWithShipper", async (NorthwindContext db) =>
+{
+    Order order = await GetOrder(46, db, o => o.ShipViaNavigation);
+    return new { OrderId = order.OrderId, ShipVia = order.ShipVia, Shipper = order.ShipViaNavigation };
+});
+
+app.MapGet("getOrderWithCustomer", async (NorthwindContext db) =>
+{
+    Order order = await GetOrder(46, db, o => o.Customer);
+    return new { OrderId = order.OrderId, Customer = order.Customer};
 });
 
 app.Run();
+
+async Task<Order> GetOrder(int orderId, NorthwindContext db, params Expression<Func<Order, object>>[] includes)
+{
+    var baseQuery = db.Orders
+        .Where(o => o.OrderId == orderId);
+
+    if (includes.Any())
+    {
+        foreach (var include in includes)
+        {
+            baseQuery = baseQuery.Include(include);
+        }
+    }
+
+    var order = await baseQuery
+        .FirstAsync();
+
+    return order;
+}
